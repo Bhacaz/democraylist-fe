373e68c00d24d80ef29344463ae6cd3bf068af27
Jean-Francis Bastien <bhacaz@gmail.com>
2020-04-04 23:54:01 -0400

    Add deploy workflow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DemocraticPlaylistFE'`, () => {
+  it(`should have as title 'democraylist-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DemocraticPlaylistFE');
+    expect(app.title).toEqual('democraylist-fe');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DemocraticPlaylistFE!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to democraylist-fe!');
   });
 });
