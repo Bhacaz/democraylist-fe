8100229d8469119960fa04d6379ec95cace49807
Jean-Francis Bastien <bhacaz@gmail.com>
2020-04-10 23:52:28 -0400

    Add forbideen interceptor

--- src/app/democraylist/auth-interceptor.ts
+++ src/app/democraylist/auth-interceptor.ts
@@ -3,12 +3,25 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
+import {Router} from '@angular/router';
+import {catchError} from 'rxjs/operators';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private router: Router) { }
+
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+    // handle your auth error or rethrow
+    if (err.status === 401 || err.status === 403) {
+      localStorage.removeItem('access_token');
+      this.router.navigateByUrl(`/login`);
+      return of(err.message);
+    }
+    return throwError(err);
+  }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
@@ -16,6 +29,6 @@ export class AuthInterceptor implements HttpInterceptor {
       }
     });
 
-    return next.handle(request);
+    return next.handle(request).pipe(catchError(x => this.handleAuthError(x)));
   }
 }
