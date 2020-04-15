9277dc7ef607aa290286fae1db22084094d11272
Jean-Francis Bastien <bhacaz@gmail.com>
2020-04-14 22:30:26 -0400

    Add vapid prod key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://localhost:3000/'
-  // backendUrl: 'https://democraylist.herokuapp.com/'
+  backendUrl: 'https://democraylist.herokuapp.com/',
+  vapidPublicKey: 'BHz4P66gP1n1P0MRRC9BZVvffVfQuP3INlE6ZWvMH8tcX8AA_GFk7pVx59YJJSLRke1zNPfqly3_oB5wbTJrb6o'
 };
