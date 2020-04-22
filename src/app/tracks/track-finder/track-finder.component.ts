0cd725dbf6f891b1e8d2dbe0b5f41c40466fc555
Jean-Francis Bastien <bhacaz@gmail.com>
2020-04-22 14:03:57 -0400

    Add debounceTime for searching

--- src/app/tracks/track-finder/track-finder.component.ts
+++ src/app/tracks/track-finder/track-finder.component.ts
@@ -1,20 +1,35 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {DemocraylistService} from '../../democraylist/democraylist.service';
+import {Subject, Subscription} from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-track-finder',
   templateUrl: './track-finder.component.html',
   styleUrls: ['./track-finder.component.scss']
 })
-export class TrackFinderComponent implements OnInit {
+export class TrackFinderComponent implements OnInit, OnDestroy {
 
   searchResult;
   query: string = '';
+  queryChanged: Subject<string> = new Subject<string>();
+  private queryChangedSubscription: Subscription;
+
   @Output() trackSelected = new EventEmitter();
 
   constructor(
     private democraylistService: DemocraylistService,
-  ) { }
+  ) {
+    this.queryChangedSubscription = this.queryChanged
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(newQuery => {
+        this.query = newQuery;
+        this.searchTrack();
+      });
+  }
 
   ngOnInit(): void {
   }
@@ -36,4 +51,7 @@ export class TrackFinderComponent implements OnInit {
     this.searchResult = undefined;
   }
 
+  ngOnDestroy() {
+    this.queryChangedSubscription.unsubscribe();
+  }
 }
