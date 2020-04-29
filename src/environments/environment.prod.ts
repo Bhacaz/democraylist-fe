d08a5c6b9d5247a39e9460d6916b66f994b4915e
Jean-Francis Bastien <bhacaz@gmail.com>
2020-04-28 22:23:33 -0400

    Put back env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // backendUrl: 'https://democraylist.herokuapp.com/',
-  // vapidPublicKey: 'BHz4P66gP1n1P0MRRC9BZVvffVfQuP3INlE6ZWvMH8tcX8AA_GFk7pVx59YJJSLRke1zNPfqly3_oB5wbTJrb6o'
-  backendUrl: 'http://localhost:3000/',
-  vapidPublicKey: 'BEWrjXKrN7b4hUiqIV-cLYJvUjTI_ntQXV3kz7ZIWgBnbzSl-jvG8hzamjK71cKsBaSrF0pwwdl6TOEH9Lguk4Q'
+  backendUrl: 'https://democraylist.herokuapp.com/',
+  vapidPublicKey: 'BHz4P66gP1n1P0MRRC9BZVvffVfQuP3INlE6ZWvMH8tcX8AA_GFk7pVx59YJJSLRke1zNPfqly3_oB5wbTJrb6o'
 };
